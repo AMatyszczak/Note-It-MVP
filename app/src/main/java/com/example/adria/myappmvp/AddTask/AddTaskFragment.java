5326621dc36e2d8ada4a10bcafcb2515186a23b8
adria <adrianmatyszczak@gmail.com>
2018-05-05 17:56:21 +0200

    Added functions to create delegate to Presenter

--- app/src/main/java/com/example/adria/myappmvp/AddTask/AddTaskFragment.java
+++ app/src/main/java/com/example/adria/myappmvp/AddTask/AddTaskFragment.java
@@ -1,12 +1,17 @@
 package com.example.adria.myappmvp.AddTask;
 
+import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.adria.myappmvp.R;
@@ -14,8 +19,8 @@ import com.example.adria.myappmvp.R;
 
 public class AddTaskFragment extends Fragment implements AddTaskContract.View {
 
-    private TextView mTitle;
-    private TextView mDescription;
+    private EditText mTitle;
+    private EditText mDescription;
     private AddTaskContract.Presenter mPresenter;
 
 
@@ -31,13 +36,27 @@ public class AddTaskFragment extends Fragment implements AddTaskContract.View {
     }
 
     @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        FloatingActionButton fab = (FloatingActionButton) getActivity().findViewById(R.id.fab_add_task);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mPresenter.addTask();
+            }
+        });
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.addtask_frag, container, false);
 
-        mTitle = (TextView)root.findViewById(R.id.title);
-        mDescription = (TextView)root.findViewById(R.id.descrption);
+        mTitle = (EditText)root.findViewById(R.id.title);
+        mDescription = (EditText)root.findViewById(R.id.description);
+
 
         return root;
     }
@@ -45,6 +64,25 @@ public class AddTaskFragment extends Fragment implements AddTaskContract.View {
     @Override
     public void setPresenter(AddTaskContract.Presenter presenter) {
         mPresenter = presenter;
+    }
+
+    @Override
+    public String getTitle()
+    {
+        return mTitle.getText().toString();
+    }
+
+    @Override
+    public String getDescription()
+    {
+        return mDescription.getText().toString();
+    }
+
+    @Override
+    public void showTasks()
+    {
+        getActivity().setResult(Activity.RESULT_OK);
+        getActivity().finish();
     }
 
 
