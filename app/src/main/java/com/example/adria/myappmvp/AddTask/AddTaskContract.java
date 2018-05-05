7fe84ef22458cae29da80e5f145af059980f55a7
adria <adrianmatyszczak@gmail.com>
2018-05-05 17:54:09 +0200

    Functions added

--- app/src/main/java/com/example/adria/myappmvp/AddTask/AddTaskContract.java
+++ app/src/main/java/com/example/adria/myappmvp/AddTask/AddTaskContract.java
@@ -9,11 +9,15 @@ public interface AddTaskContract
     interface View
     {
         void setPresenter(AddTaskContract.Presenter presenter);
+        public String getTitle();
+        public String getDescription();
+        public void showTasks();
+
     }
 
     interface Presenter
     {
-
+        void addTask();
     }
 
 }
