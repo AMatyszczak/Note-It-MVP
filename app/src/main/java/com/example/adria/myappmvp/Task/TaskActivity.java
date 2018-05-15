0bfe46c8d7bb0c98a6b4437f978697359a21ce98
adria <adrianmatyszczak@gmail.com>
2018-05-15 18:15:35 +0200

    Deleted toolbar

--- app/src/main/java/com/example/adria/myappmvp/Task/TaskActivity.java
+++ app/src/main/java/com/example/adria/myappmvp/Task/TaskActivity.java
@@ -2,9 +2,6 @@ package com.example.adria.myappmvp.Task;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.example.adria.myappmvp.R;
 import com.example.adria.myappmvp.data.TaskRepository;
@@ -19,13 +16,9 @@ public class TaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.task_act);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         TaskFragment taskFragment = (TaskFragment)getSupportFragmentManager().findFragmentById(R.id.Taskfragment);
         TaskRepository taskRepository = TaskRepository.getINSTANCE(getApplication());
         mPresenter = new TaskPresenter(taskFragment, taskRepository );
-
-
     }
 }
