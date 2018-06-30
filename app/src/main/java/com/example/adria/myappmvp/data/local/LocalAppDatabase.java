8b7f6016bbd9d7c2e746c52072cf94f2d2ac1a0a
adria <adrianmatyszczak@gmail.com>
2018-07-01 01:45:39 +0200

    Moved to the new package

--- app/src/main/java/com/example/adria/myappmvp/data/local/LocalAppDatabase.java
+++ app/src/main/java/com/example/adria/myappmvp/data/local/LocalAppDatabase.java
@@ -1,30 +1,32 @@
-package com.example.adria.myappmvp.data;
+package com.example.adria.myappmvp.data.local;
 
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
 
+import com.example.adria.myappmvp.data.Task;
+
 /**
  * Created by adria on 01.05.2018.
  */
 @Database(entities = {Task.class}, version = 1, exportSchema = false)
-public abstract class AppDatabase extends RoomDatabase
+public abstract class LocalAppDatabase extends RoomDatabase
 {
     public abstract TaskDao taskDao();
 
-    private static AppDatabase INSTANCE;
+    private static LocalAppDatabase INSTANCE;
 
-    static AppDatabase getDatabase(Context context)
+    static LocalAppDatabase getDatabase(Context context)
     {
         if(INSTANCE == null)
         {
-            synchronized (AppDatabase.class)
+            synchronized (LocalAppDatabase.class)
             {
                 if(INSTANCE == null)
                 {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                            AppDatabase.class, "App_Database").allowMainThreadQueries().build();
+                            LocalAppDatabase.class, "App_Database").allowMainThreadQueries().fallbackToDestructiveMigration().build();
                 }
             }
         }
