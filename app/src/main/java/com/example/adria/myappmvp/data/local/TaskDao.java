3daa4db7a3d957454cb864c41de56e5c46362fa1
adria <adrianmatyszczak@gmail.com>
2018-07-12 19:31:36 +0200

    Added TaskDao

--- app/src/main/java/com/example/adria/myappmvp/data/local/TaskDao.java
+++ app/src/main/java/com/example/adria/myappmvp/data/local/TaskDao.java
@@ -3,38 +3,16 @@ package com.example.adria.myappmvp.data.local;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
-import android.arch.persistence.room.OnConflictStrategy;
-import android.arch.persistence.room.Query;
-import android.arch.persistence.room.Update;
 
-import com.example.adria.myappmvp.data.Note;
+import com.example.adria.myappmvp.data.Task;
 
-import java.util.List;
-
-/**
- * Created by adria on 01.05.2018.
- */
 @Dao
 public interface TaskDao
 {
-    @Query("SELECT * FROM Note ORDER BY position ASC")
-    List<Note> getNotes();
+    @Insert
+    void updateTask(Task task);
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    void insertNote(Note note);
+   @Delete
+   void deleteTask(Task task);
 
-    @Query("SELECT * From Note WHERE id = :id")
-    Note getNoteFromId(String id);
-
-    @Query("DELETE FROM Note")
-    void deleteAllNotes();
-
-    @Query("SELECT COUNT() FROM Note")
-    int getNoteCount();
-
-    @Delete
-    void deleteNote(Note note);
-
-    @Update
-    void updateNote(Note note);
 }
