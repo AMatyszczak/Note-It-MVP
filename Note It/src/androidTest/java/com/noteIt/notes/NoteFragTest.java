0a50bf6d0a1d9aa8ab2baf54537c4a6f656fee28
adria <adrianmatyszczak@gmail.com>
2018-07-17 16:26:13 +0200

    Added and refactored test, now contain tasks too

--- Note It/src/androidTest/java/com/noteIt/notes/NoteFragTest.java
+++ Note It/src/androidTest/java/com/noteIt/notes/NoteFragTest.java
@@ -2,6 +2,7 @@ package com.noteIt.notes;
 
 
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -9,6 +10,7 @@ import android.support.test.runner.AndroidJUnit4;
 import com.noteIt.Injection;
 import com.noteIt.R;
 
+import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,10 +19,18 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasSibling;
+import static android.support.test.espresso.matcher.ViewMatchers.isChecked;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+import static android.support.test.internal.util.Checks.checkArgument;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsNot.not;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -29,8 +39,13 @@ public class NoteFragTest
 {
 
     private static final String TITLE = "TITLE";
+    private static final String TITLE2 = "TITLE2";
     private static final String DESCRIPTION = "DESCRIPTION";
-    private static final String DESCRITPION2 = "DESCRIPTION2";
+    private static final String DESCRIPTION2 = "DESCRIPTION2";
+
+    private static final String TASKDESC = "TASKDESC";
+    private static final String TASKDESC2 = "TASKDESC2";
+    private static final String TASKDESC3 = "TASKDESC3";
 
     @Rule
     public ActivityTestRule<NoteActivity> mActivityTaskRule = new ActivityTestRule<NoteActivity>(NoteActivity.class)
@@ -44,28 +59,59 @@ public class NoteFragTest
 
 
     @Test
-    public void editTask()
+    public void editNote()
     {
-        createTask(TITLE,DESCRIPTION);
-
+        createNote(TITLE,DESCRIPTION, "", TASKDESC2);
         onView(withText(TITLE)).perform(click());
+        onView(withId(R.id.detail_note_title)).check(matches(isDisplayed()));
 
-        onView(withId(R.id.edit_note_title)).check(matches(isDisplayed()));
+        onView(withId(R.id.detail_note_description)).perform(clearText()).perform(typeText(DESCRIPTION2));
+        onView(withId(R.id.detail_note_title)).perform(clearText()).perform(typeText(TITLE2));
 
+        onView(allOf(isDisplayed(),withId(R.id.task_recycler_view_detail)))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0,typeText(TASKDESC3)));
+        onView(allOf(isDisplayed(), withId(R.id.task_clear_button), hasSibling(withText(TASKDESC2)))).perform(click());
 
-        onView(withId(R.id.edit_note_description)).perform(clearText()).perform(typeText(DESCRITPION2));
+        clickAddNewTask();
+        onView(allOf(isDisplayed(), withId(R.id.task_recycler_view_detail)))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(1,typeText(TASKDESC) ));
+        clickCheckBoxForTask(TASKDESC);
+
         onView(withId(R.id.fab_note_detail)).perform(click());
 
-        onView(withText(DESCRITPION2)).check(matches(isDisplayed()));
-
+        onView(withText(DESCRIPTION2)).check(matches(isDisplayed()));
+        onView(withText(TITLE2)).check(matches(isDisplayed()));
+        onView(allOf(withId(R.id.task_layout_text), withText(TASKDESC3))).check(matches(isDisplayed()));
+        onView(allOf(withId(R.id.task_layout_text), withText(TASKDESC2))).check(doesNotExist());
+        onView(allOf(withId(R.id.task_layout_text), withText(TASKDESC))).check(matches(isDisplayed()));
+        onView(allOf(withId(R.id.task_layout_checkbox), hasSibling(withText(TASKDESC)))).check(matches(isChecked()));
     }
 
-    private void createTask(String title, String descripion)
+    private void clickCheckBoxForTask(String desc) {
+        onView(Matchers.allOf(withId(R.id.task_checkbox), hasSibling(withText(desc)))).perform(click());
+    }
+
+    private void clickAddNewTask()
+    {
+        onView(withId(R.id.add_task_text_view)).perform(click());
+    }
+
+    private void createNote(String title, String description, String... taskDesc)
     {
         onView(withId(R.id.fab_note)).perform(click());
         onView(withId(R.id.add_note_title)).perform(typeText(title));
-        onView(withId(R.id.add_note_description)).perform(typeText(descripion));
+        onView(withId(R.id.add_note_description)).perform(typeText(description));
+
+        for(int i = 0 ; i < taskDesc.length; i++)
+        {
+            clickAddNewTask();
+            onView(allOf(isDisplayed(), withId(R.id.task_recycler_view_add)))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(i, typeText(taskDesc[i])));
+        }
+
         onView(withId(R.id.fab_add_note)).perform(click());
+
+
     }
 
 }
